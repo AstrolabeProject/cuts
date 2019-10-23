ee71a0d3e0ba46bc435a56964c955e0d49756d20
Tom Hicks <hickst@tohono.com>
2019-10-23 09:05:49 -0700

    Set reloading in config file. Modify logging record.

--- config/gunicorn.py
+++ config/gunicorn.py
@@ -2,4 +2,6 @@
 
 bind = '0.0.0.0:8000'
 accesslog = '-'
-access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" in %(D)sµs'
+access_log_format = '%(h)s %(u)s %(t)s "%(r)s" %(s)s %(b)s in %(D)sµs (%(L)ss)'
+reload = True
+workers = 2
