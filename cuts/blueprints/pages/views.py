291f5303270377624a101d9f6c7fe7a4c7f8d6f9
Tom Hicks <hickst@tohono.com>
2019-10-24 21:31:02 -0700

    Move help pages to pages blueprint, update views.

--- cuts/blueprints/pages/views.py
+++ cuts/blueprints/pages/views.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template, abort
-# from jinja2 import TemplateNotFound
+from jinja2 import TemplateNotFound
 
 pages = Blueprint('pages', __name__, template_folder='templates')
 
 
+# General pages
+#
 @pages.route('/')
 def index():
     return render_template('pages/index.html')
@@ -17,6 +19,23 @@ def about():
 @pages.route('/license')
 def license():
     return render_template('pages/license.html')
+
+
+# Help methods
+#
+
+# general routing for help pages
+@pages.route('/help/<topic>')
+def show_help (topic):
+    try:
+        return render_template('help/%s.html' % topic)
+    except TemplateNotFound:
+        abort(404)
+
+# main help page
+@pages.route('/help')
+def help():
+    return render_template('help/help.html')
 
 
 # default routing for pages
