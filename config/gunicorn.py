7ce058387df7f9f34735a9e7f79c7bb28d9d9931
Tom Hicks <hickst@tohono.com>
2019-11-04 16:51:45 -0700

    Convert to using gevent to get multiple, longer-running, asynchronous workers.

--- config/gunicorn.py
+++ config/gunicorn.py
@@ -5,4 +5,10 @@ accesslog = '-'
 access_log_format = '%(h)s %(u)s %(t)s "%(r)s" %(s)s %(b)s in %(D)sµs (%(L)ss)'
 errorlog = '-'
 reload = True
-workers = 1
+
+# workers configuration
+workers = 2
+worker_class = 'gevent'
+worker_connections = 100
+threads = 8
+timeout = 120
