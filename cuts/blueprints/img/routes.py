39287c5767ca85eb08cedee71797407cba7b246d
Tom Hicks <hickst@tohono.com>
2020-01-13 17:12:20 -0700

    Embed method doc strings.

--- cuts/blueprints/img/routes.py
+++ cuts/blueprints/img/routes.py
@@ -1,3 +1,9 @@
+#
+# Top-level Flask routing module: answers requests or spawns Celery task to do it.
+#
+#   Written by: Tom Hicks. 11/14/2019.
+#   Last Modified: Embed method doc strings.
+#
 from flask import Blueprint, jsonify, request
 # from flask_cors import CORS
 from cuts.blueprints.img import exceptions
@@ -11,30 +17,33 @@ img = Blueprint('img', __name__, template_folder='templates')
 # Full image methods
 #
 
-# list all FITS images found in the image directory
 @img.route('/img/list')
 def img_list ():
+    """ List all FITS images found in the image directory or a sub-collection. """
     # required to avoid circular imports
     from cuts.blueprints.img.tasks import list_images
     return list_images()
 
-# fetch a specific image by filename
+
 @img.route('/img/<filename>', methods=['GET'])
 def img_fetch (filename):
+    """ Fetch a specific image by filename. """
     # required to avoid circular imports
     from cuts.blueprints.img.tasks import fetch_image
     return fetch_image(filename)
 
-# tell whether the specified image contains the specified coordinate
+
 @img.route('/img/contains/<filename>', methods=['GET'])
 def img_contains (filename):
+    """ Tell whether the specified image contains the specified coordinate. """
     # required to avoid circular imports
     from cuts.blueprints.img.tasks import image_contains
     return image_contains(filename, request.args)
 
-# return the corner coordinates of the specified image
+
 @img.route('/img/corners/<filename>', methods=['GET'])
 def img_corners (filename):
+    """ Return the corner coordinates of the specified image. """
     # required to avoid circular imports
     from cuts.blueprints.img.tasks import image_corners
     return image_corners(filename)
@@ -44,31 +53,33 @@ def img_corners (filename):
 # Image cutout methods
 #
 
-# list all existing cutouts in the cache directory
 @img.route('/img/co/list')
 def co_list ():
+    """ List all existing cutouts in the cache directory. """
     # required to avoid circular imports
     from cuts.blueprints.img.tasks import list_cutouts
     return list_cutouts()
 
-# fetch a specific cutout by filename
+
 @img.route('/img/co/<filename>', methods=['GET'])
 def co_fetch (filename):
+    """ Fetch a specific cutout by filename. """
     # required to avoid circular imports
     from cuts.blueprints.img.tasks import fetch_cutout
     return fetch_cutout(filename)
 
 
-# Make and return an image cutout
 @img.route('/img/co/cutout')
 def co_cutout ():
+    """ Make and return an image cutout. """
     # required to avoid circular imports
     from cuts.blueprints.img.tasks import get_cutout
     return get_cutout(request.args)
 
-# Make and return an image cutout for an image in a certain bandwidth
+
 @img.route('/img/co/cutout_by_filter')
 def co_cutout_by_filter ():
+    """ Make and return an image cutout for an image in a certain bandwidth. """
     # required to avoid circular imports
     from cuts.blueprints.img.tasks import get_cutout_by_filter
     return get_cutout_by_filter(request.args)
@@ -76,10 +87,13 @@ def co_cutout_by_filter ():
 
 @img.route('/echo')
 def echo ():
+    """ Echo the Request arguments as a JSON data structure. """
     return jsonify(request.args)
+
 
 @img.route('/admin/show_cache')
 def admin_show_cache ():
+    """ Show the current contents of the image cache. """
     # required to avoid circular imports
     from cuts.blueprints.img.tasks import show_cache
     return show_cache(request.args)
