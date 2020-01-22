4b1d9b53f97a54e831ce1f5813dfdf445a7aaaac
Tom Hicks <hickst@tohono.com>
2020-01-21 21:10:00 -0700

    Continue test development for image manager.

--- tests/img/test_image_manager.py
+++ tests/img/test_image_manager.py
@@ -1,8 +1,8 @@
-import cuts.blueprints.img.image_manager as imgr
-
 import os
-# import sys
 import pytest
+
+import cuts.blueprints.img.image_manager as imgr
+from config.settings import FITS_MIME_TYPE, IMAGES_DIR, IMAGE_EXTS
 
 class TestUtils(object):
 
@@ -53,66 +53,86 @@ class TestUtils(object):
         assert imgr.cache_key_from_metadata({'filepath': '/some/path/'}) == '/some/path/'
 
 
-        def test_collection_from_dirpath(self):
-            return True
+    def test_collection_from_dirpath(self):
+        assert imgr.collection_from_dirpath('') == ''
+        assert imgr.collection_from_dirpath(os.path.join(IMAGES_DIR, '/')) == ''
+        assert imgr.collection_from_dirpath(os.path.join(IMAGES_DIR, '/x')) == ''
+        assert imgr.collection_from_dirpath(os.path.join(IMAGES_DIR, '/x/y/z/')) == ''
+        assert imgr.collection_from_dirpath(os.path.join(IMAGES_DIR, 'x')) == 'x'
+        assert imgr.collection_from_dirpath(os.path.join(IMAGES_DIR, 'xx/yyy')) == 'xx/yyy'
+        assert imgr.collection_from_dirpath(os.path.join(IMAGES_DIR, 'x/y/z')) == 'x/y/z'
+        assert imgr.collection_from_dirpath(os.path.join(IMAGES_DIR, 'x/y/z/')) == 'x/y/z'
 
 
-        def test_collection_from_filepath(self):
-            return True
+    def test_collection_from_filepath(self):
+        return True
 
 
-        def test_extract_metadata(self):
-            return True
+    def test_extract_metadata(self):
+        return True
 
 
-        def test_fetch_metadata(self):
-            return True
+    def test_fetch_metadata(self):
+        return True
 
 
-        def test_fits_file_exists(self):
-            return True
+    def test_fits_file_exists(self):
+        return True
 
 
-        def test_gen_collection_names(self):
-            return True
+    def test_gen_collection_names(self):
+        return True
 
 
-        def test_get_metadata(self):
-            return True
+    def test_get_metadata(self):
+        return True
 
 
-        def test_image_dir_from_collection(self):
-            return True
+    def test_image_dir_from_collection(self):
+        coll = 'coll'
+        goods = 'JADES/goods'
+        pics = 'Pics'
+        assert imgr.image_dir_from_collection() == IMAGES_DIR
+        assert imgr.image_dir_from_collection(None) == IMAGES_DIR
+        assert imgr.image_dir_from_collection(coll).startswith(IMAGES_DIR)
+        assert imgr.image_dir_from_collection(coll).endswith(os.sep+coll)
+        assert imgr.image_dir_from_collection(goods).startswith(IMAGES_DIR)
+        assert imgr.image_dir_from_collection(goods).endswith(os.sep+goods)
+
+        assert imgr.image_dir_from_collection(None, pics) == pics
+        assert imgr.image_dir_from_collection(coll, pics).startswith(pics)
+        assert imgr.image_dir_from_collection(coll, pics).endswith(os.sep+coll)
+        assert imgr.image_dir_from_collection(goods, pics).startswith(pics)
+        assert imgr.image_dir_from_collection(goods, pics).endswith(os.sep+goods)
 
 
-        def test_is_image_file(self):
-            return True
+    def test_is_image_file(self):
+        return True
 
 
-        def test_is_image_header(self):
-            return True
+    def test_is_image_header(self):
+        return True
 
 
-        def test_list_collections(self):
-            return True
+    def test_list_collections(self):
+        return True
 
 
-        def test_list_fits_paths(self):
-            return True
+    def test_list_fits_paths(self):
+        return True
 
 
-        def test_metadata_contains(self):
-            return True
+    def test_metadata_contains(self):
+        return True
 
 
-        def test_put_metadata(self):
-            return True
+    def test_put_metadata(self):
+        return True
 
 
-        def test_return_image(self):
-            return True
+    def test_return_image(self):
+        return True
 
 
-        def test_store_metadata(self):
-            return True
-
+    def test_store_metadata(self):
+        return True
