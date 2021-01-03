43b3faa5756f18c3128ca6fad9278ca51345bfb4
Tom Hicks <hickst@tohono.com>
2021-01-02 17:22:47 -0700

    Fix: explicitly import current_app.

--- cuts/blueprints/img/arg_utils.py
+++ cuts/blueprints/img/arg_utils.py
@@ -2,8 +2,9 @@
 # Utilities for argument parsing and validataion.
 #
 #   Written by: Tom Hicks. 12/28/2020.
-#   Last Modified: Initial split of argument handling/parsing from tasks.
+#   Last Modified: Fix: explicitly import current_app.
 #
+from flask import current_app
 
 from astropy import units as u
 from astropy.coordinates import SkyCoord
@@ -21,7 +22,7 @@ def parse_collection_arg (args, required=False):
     if ((coll is None) or (not coll.strip())):    # if no collection or empty collection
         if (required):
             errMsg = "A collection name must be specified, via the 'collection' argument"
-            # current_app.logger.error(errMsg)  
+            current_app.logger.error(errMsg)
             raise exceptions.RequestException(errMsg)
         else:
             return None
@@ -36,7 +37,7 @@ def parse_coordinate_args (args):
     raStr = args.get('ra')
     if (not raStr):
         errMsg = "Right ascension must be specified, via the 'ra' argument"
-        # current_app.logger.error(errMsg)  
+        current_app.logger.error(errMsg)
         raise exceptions.RequestException(errMsg)
     else:
         ra = float(raStr)
@@ -45,7 +46,7 @@ def parse_coordinate_args (args):
     decStr = args.get('dec')
     if (not decStr):
         errMsg = "Declination must be specified, via the 'dec' argument"
-        # current_app.logger.error(errMsg)  
+        current_app.logger.error(errMsg)
         raise exceptions.RequestException(errMsg)
     else:
         dec = float(decStr)
@@ -91,7 +92,7 @@ def parse_cutout_size (args, required=False):
     if (co_args.get('size') is None):       # if no size given
         if (required):                      # if size argument required
             errMsg = "A radius size (one of 'radius', 'sizeDeg', 'sizeArcMin', or 'sizeArcSec') must be specified."
-            # current_app.logger.error(errMsg)  
+            current_app.logger.error(errMsg)
             raise exceptions.RequestException(errMsg)
         return co_args                      # not required: return empty dictionary
 
@@ -109,7 +110,7 @@ def parse_filter_arg (args, required=False):
     if ((filt is None) or (not filt.strip())):    # if no filter or empty filter
         if (required):
             errMsg = "An image filter must be specified, via the 'filter' argument"
-            # current_app.logger.error(errMsg)  
+            current_app.logger.error(errMsg)
             raise exceptions.RequestException(errMsg)
         else:
             return None
@@ -135,7 +136,7 @@ def parse_id_arg (args, required=True):
         return None
     else:
         errMsg = "A record ID must be specified, via the 'id' argument"
-        # current_app.logger.error(errMsg)  
+        current_app.logger.error(errMsg)
         raise exceptions.RequestException(errMsg)
 
 
@@ -148,7 +149,7 @@ def parse_ipath_arg (args, required=False):
     if ((ipath is None) or (not ipath.strip())):    # if no path or empty path
         if (required):
             errMsg = "A valid image path must be specified, via the 'path' argument"
-            # current_app.logger.error(errMsg)  
+            current_app.logger.error(errMsg)
             raise exceptions.RequestException(errMsg)
         else:
             return None
