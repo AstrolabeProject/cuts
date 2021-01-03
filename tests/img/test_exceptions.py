9e77dba1ed6925ac3a9767653e986ce1256385d0
Tom Hicks <hickst@tohono.com>
2021-01-02 19:06:02 -0700

    Update for full coverage.

--- tests/img/test_exceptions.py
+++ tests/img/test_exceptions.py
@@ -9,41 +9,43 @@ class TestExceptions(object):
     BAD_FILE = 'Image XXX.bad Not Found'
     FAULT = 'Something went wrong'
 
-    def test_request_exception(self):
-        reqex = xcpt.RequestException(self.BAD_REQUEST)
+    def test_processing_error_default(self):
+        reqex = xcpt.ProcessingError(self.FAULT)
         print(reqex)
         print(type(reqex))
-        assert reqex.error_code == 400
+        assert reqex.error_code == xcpt.ProcessingError.ERROR_CODE
         rdict = reqex.to_dict()
         assert 'message' in rdict
-        assert rdict.get('message') == self.BAD_REQUEST
+        assert rdict.get('message') == self.FAULT
         rtup = reqex.to_tuple()
-        assert rtup[0] == self.BAD_REQUEST
-        assert rtup[1] == 400
+        assert rtup[0] == self.FAULT
+        assert rtup[1] == xcpt.ProcessingError.ERROR_CODE
 
 
-    def test_request_exception_withcode(self):
-        reqex= xcpt.RequestException(self.BAD_REQUEST, 555)
-        assert reqex.error_code == 555
+    def test_processing_error(self):
+        reqex = xcpt.ProcessingError(self.FAULT, 100)
+        print(reqex)
+        print(type(reqex))
+        assert reqex.error_code == 100
         rdict = reqex.to_dict()
         assert 'message' in rdict
-        assert rdict.get('message') == self.BAD_REQUEST
+        assert rdict.get('message') == self.FAULT
         rtup = reqex.to_tuple()
-        assert rtup[0] == self.BAD_REQUEST
-        assert rtup[1] == 555
+        assert rtup[0] == self.FAULT
+        assert rtup[1] == 100
 
 
     def test_image_not_found(self):
         reqex = xcpt.ImageNotFound(self.BAD_FILE)
         print(reqex)
         print(type(reqex))
-        assert reqex.error_code == 404
+        assert reqex.error_code == xcpt.ImageNotFound.ERROR_CODE
         rdict = reqex.to_dict()
         assert 'message' in rdict
         assert rdict.get('message') == self.BAD_FILE
         rtup = reqex.to_tuple()
         assert rtup[0] == self.BAD_FILE
-        assert rtup[1] == 404
+        assert rtup[1] == xcpt.ImageNotFound.ERROR_CODE
 
 
     def test_image_not_found_withcode(self):
@@ -57,17 +59,41 @@ class TestExceptions(object):
         assert rtup[1] == 99
 
 
+    def test_request_exception(self):
+        reqex = xcpt.RequestException(self.BAD_REQUEST)
+        print(reqex)
+        print(type(reqex))
+        assert reqex.error_code == xcpt.RequestException.ERROR_CODE
+        rdict = reqex.to_dict()
+        assert 'message' in rdict
+        assert rdict.get('message') == self.BAD_REQUEST
+        rtup = reqex.to_tuple()
+        assert rtup[0] == self.BAD_REQUEST
+        assert rtup[1] == xcpt.RequestException.ERROR_CODE
+
+
+    def test_request_exception_withcode(self):
+        reqex= xcpt.RequestException(self.BAD_REQUEST, 555)
+        assert reqex.error_code == 555
+        rdict = reqex.to_dict()
+        assert 'message' in rdict
+        assert rdict.get('message') == self.BAD_REQUEST
+        rtup = reqex.to_tuple()
+        assert rtup[0] == self.BAD_REQUEST
+        assert rtup[1] == 555
+
+
     def test_server_exception(self):
         reqex = xcpt.ServerError(self.FAULT)
         print(reqex)
         print(type(reqex))
-        assert reqex.error_code == 500
+        assert reqex.error_code == xcpt.ServerError.ERROR_CODE
         rdict = reqex.to_dict()
         assert 'message' in rdict
         assert rdict.get('message') == self.FAULT
         rtup = reqex.to_tuple()
         assert rtup[0] == self.FAULT
-        assert rtup[1] == 500
+        assert rtup[1] == xcpt.ServerError.ERROR_CODE
 
 
     def test_server_exception_withcode(self):
@@ -79,3 +105,27 @@ class TestExceptions(object):
         rtup = reqex.to_tuple()
         assert rtup[0] == self.FAULT
         assert rtup[1] == 7
+
+
+    def test_unsupported_type(self):
+        reqex = xcpt.UnsupportedType(self.FAULT)
+        print(reqex)
+        print(type(reqex))
+        assert reqex.error_code == xcpt.UnsupportedType.ERROR_CODE
+        rdict = reqex.to_dict()
+        assert 'message' in rdict
+        assert rdict.get('message') == self.FAULT
+        rtup = reqex.to_tuple()
+        assert rtup[0] == self.FAULT
+        assert rtup[1] == xcpt.UnsupportedType.ERROR_CODE
+
+
+    def test_unsupported_type_withcode(self):
+        reqex= xcpt.UnsupportedType(self.FAULT, 77)
+        assert reqex.error_code == 77
+        rdict = reqex.to_dict()
+        assert 'message' in rdict
+        assert rdict.get('message') == self.FAULT
+        rtup = reqex.to_tuple()
+        assert rtup[0] == self.FAULT
+        assert rtup[1] == 77
