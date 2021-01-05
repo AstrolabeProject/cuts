e7bd336a71d8c538a6bc504365afd1e44bf4fa43
Tom Hicks <hickst@tohono.com>
2021-01-05 14:16:16 -0700

    Add two redundant alternate tests as a test.

--- tests/img/test_tasks.py
+++ tests/img/test_tasks.py
@@ -7,18 +7,39 @@ from cuts.blueprints.img.exceptions import ImageNotFound, RequestException, Serv
 
 class TestTasks(object):
 
-    def test_fetch_image_noid(self):
+    id_emsg = "A record ID must be specified"
+    id_nf_emsg = ".* Image with image ID '{0}' not found in database"
+
+
+    def test_img_fetch_noid(self, client):
+        """ No ID argument. """
         args = {}
-        emsg="A record ID must be specified"
-        with pytest.raises(RequestException, match=emsg) as reqex:
-            tasks.fetch_image(args)
+        with pytest.raises(RequestException, match=self.id_emsg) as reqex:
+            resp = tasks.fetch_image(args)
+            print(resp)
 
 
-    def test_fetch_image_badid(self):
+    def test_img_fetch_badid(self, client):
+        """ No such record with given ID. """
         args = { 'id': '9999' }
-        emsg="Image with image ID .* not found"
-        with pytest.raises(ImageNotFound, match=emsg) as reqex:
-            tasks.fetch_image(args)
+        errmsg = self.id_nf_emsg.format('9999')
+        with pytest.raises(ImageNotFound, match=errmsg) as reqex:
+            resp = tasks.fetch_image(args)
+            print(resp)
+
+
+    # def test_fetch_image_noid(self):
+    #     args = {}
+    #     emsg="A record ID must be specified"
+    #     with pytest.raises(RequestException, match=emsg) as reqex:
+    #         tasks.fetch_image(args)
+
+
+    # def test_fetch_image_badid(self):
+    #     args = { 'id': '9999' }
+    #     emsg="Image with image ID .* not found"
+    #     with pytest.raises(ImageNotFound, match=emsg) as reqex:
+    #         tasks.fetch_image(args)
 
 
     def test_fetch_image_by_filter_nofilt_required(self):
