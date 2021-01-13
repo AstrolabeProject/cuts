1065405068df7645c39a3744a391628e71e23997
Tom Hicks <hickst@tohono.com>
2021-01-13 13:09:32 -0700

    Add tests for PostgreSQL base class.

--- tests/img/test_pg_sql_base.py
+++ tests/img/test_pg_sql_base.py
@@ -22,10 +22,10 @@ class TestPostgreSQLBase(object):
     nouri_dbconfig_tstfyl   = f"{TEST_RESOURCES_DIR}/test-dbconfig-no-uri.ini"
     nosuch_tstfyl           = f"{TEST_RESOURCES_DIR}/NOSUCHFILE"
 
-    args = { 'debug': True, 'verbose': True, 'TOOL_NAME': 'TestPostgreSQLBase' }
+    cleanid_emsg = "Identifier to be cleaned cannot be empty or None."
 
 
-    # base = PostgreSQLBase(test_args)          # instance of class under tests
+    base = PostgreSQLBase(test_args)        # instance of class under tests
 
 
     def test_load_sql_db_config_bad(self):
@@ -53,3 +53,59 @@ class TestPostgreSQLBase(object):
         print(dbconf)
         assert dbconf is not None
         assert len(dbconf) >= 7               # specific to this test file
+
+
+
+    def test_clean_id_badargs(self):
+        with pytest.raises(ServerError, match=self.cleanid_emsg):
+            self.base.clean_id(None)
+        with pytest.raises(ServerError, match=self.cleanid_emsg):
+            self.base.clean_id('')
+
+
+    def test_clean_id(self):
+        assert self.base.clean_id('a') == 'a'
+        assert self.base.clean_id('abc') == 'abc'
+        assert self.base.clean_id('A_B_C') == 'A_B_C'
+        assert self.base.clean_id('coll22') == 'coll22'
+
+
+
+    def test_execute_sql(self):
+        self.base.execute_sql('SELECT (%s)', [1])
+
+
+    def test_fetch_row(self):
+        row = self.base.fetch_row('SELECT id, obs_creator_name from sia.jtest where id = (%s)', [1])
+        assert row is not None
+        assert row != []
+        assert row[0] == 1
+        assert row[1] == 'JWST'
+
+
+
+    def test_fetch_rows(self):
+        rows = self.base.fetch_rows('SELECT id, obs_creator_name from sia.jtest where obs_collection = (%s)', ['XTRAS'])
+        assert rows is not None
+        assert rows != []
+        assert len(rows) == 2
+        assert len(rows[0]) == 2
+        assert rows[0][1] == 'JWST'
+
+
+
+    def test_fetch_rows_2dicts(self):
+        rows = self.base.fetch_rows_2dicts('SELECT id, obs_creator_name from sia.jtest where obs_collection = (%s)', ['XTRAS'])
+        assert rows is not None
+        assert rows != []
+        assert len(rows) == 2
+        assert rows[0] != {}
+        assert 'id' in rows[0]
+        assert 'obs_creator_name' in rows[0]
+        assert rows[0].get('obs_creator_name') == 'JWST'
+
+
+    def test_sql4_selected_fields(self):
+        assert self.base.sql4_selected_fields() == '*'
+        assert self.base.sql4_selected_fields([]) == ''
+        assert self.base.sql4_selected_fields(['a', 'b', 'c']) == 'a,b,c'
