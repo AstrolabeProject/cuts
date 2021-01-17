3b50078c3b2959667eb61394fdbe06b5b4f0b76a
Tom Hicks <hickst@tohono.com>
2021-01-17 16:22:41 -0700

    Add page tests for help pages.

--- tests/page/test_views.py
+++ tests/page/test_views.py
@@ -7,12 +7,26 @@ class TestPage(object):
         response = client.get(url_for('pages.index'))
         assert response.status_code == 200
 
+
     def test_license_page(self, client):
         """ License Terms page should respond with a success 200. """
         response = client.get(url_for('pages.license'))
         assert response.status_code == 200
 
+
     def test_about_page(self, client):
         """ About page should respond with a success 200. """
         response = client.get(url_for('pages.about'))
         assert response.status_code == 200
+
+
+    def test_help_page(self, client):
+        """ Help page should respond with a success 200. """
+        response = client.get('/help')
+        assert response.status_code == 200
+
+
+    def test_show_help(self, client):
+        """ Help <bad topic> page should respond with an error 404. """
+        response = client.get('/help/NONE')
+        assert response.status_code == 404
